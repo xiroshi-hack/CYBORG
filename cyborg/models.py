cbf4384eabf6b1f0c403e2377481ff13f2721ccd
abduazizdev <1002374m@gmail.com>
2022-10-17 13:51:14 +0500

    commit

--- cyborg/models.py
+++ cyborg/models.py
@@ -1,3 +1,6 @@
+from email.policy import default
+from statistics import mode
+from urllib import request
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -5,3 +8,10 @@ from django.contrib.auth.models import AbstractUser
 class MyUser(AbstractUser):
     is_organiser = models.BooleanField(default=False)
     is_agent = models.BooleanField(default=False)
+    
+    
+class Header(models.Model): 
+    title = models.CharField(max_length=150, default='Welcome To Cyborg')
+    text = models.CharField(max_length=200, default='BROWSE OUR POPULAR GAMES HERE')
+    btn = models.CharField(max_length=50, default='Browse Now')
+    
